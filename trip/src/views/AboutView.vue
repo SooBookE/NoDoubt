c875efd790d3d4346528b32cd6a0743daae8011e
minkyun99 <alsrud7412@naver.com>
2023-02-13 16:28:30 +0900

    20230213

---
+++ trip/src/views/AboutView.vue
@@ -0,0 +1,37 @@
+<template>
+ <v-carousel>
+  <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src" reverse-transition="fade-transition" transition="fade-transition">
+  </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+/*eslint-disable*/
+export default{
+  name:'app',
+  data(){
+    return{
+      items: [
+        {
+          src:'https://c.pxhere.com/photos/6d/df/couple_freedom_friends_friendship_fun_girl_love_man-1559779.jpg!d'
+        },
+        {
+          src:'https://trvis.r10s.com/d/strg/ctrl/26/e78e20f59fb5097c9dccaa9b4e0945268091e26a.47.1.26.2.jpg?resize=750:*'
+        },
+        {
+          src:'https://img.insight.co.kr/static/2016/07/26/700/F40AX97ZE4UQZ2FL3V7T.jpg'
+        },
+        {
+          src:'C:\Users\ds\Downloads\—Pngtree—city travel globalization_2285765.png'
+        }
+      ]
+    }
+  }
+  ,
+
+  methods: {}
+  }
+</script>
+
+<style>
+</style>
